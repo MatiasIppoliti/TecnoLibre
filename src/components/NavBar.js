28a84c00ad7fbf89c17939a5bf069f502b59e2a5
matiasippoliti <matiasippoliti@gmail.com>
2021-05-27 19:31:48 -0300

    agregaddo navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+<>
+    <navBar />
+        <h1>TecnoLibre</h1>
+
+        <p>Monitores</p>
+        <p>Teclados</p>
+        <p>Mouses</p>
+        <p>Auriculares</p>
+        <p>Gabinetes</p>
+        </>
